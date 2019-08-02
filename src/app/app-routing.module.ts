28bcf9b0ea9c426ebb1ea867be2571bc00b76520
Mandeep Sidhu <mandeep.sidhu2@gmail.com>
2019-08-02 17:56:10 +0530

    added lazy loaded module website

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./website/website.module').then(mod => mod.WebsiteModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
