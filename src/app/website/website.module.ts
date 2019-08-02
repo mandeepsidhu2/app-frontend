1633730305305cc37afb5f5a999a00e38aea4383
Mandeep Sidhu <mandeep.sidhu2@gmail.com>
2019-08-02 23:15:23 +0530

    navbar done for dashboard

--- src/app/website/website.module.ts
+++ src/app/website/website.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WebsiteRoutingModule } from './website-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, NavbarComponent],
   imports: [
     CommonModule,
     WebsiteRoutingModule
