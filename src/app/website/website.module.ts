caf36738dd0a72967a2fa9469dfc216fa51210b8
Mandeep Sidhu <mandeep.sidhu2@gmail.com>
2019-08-04 17:37:29 +0530

    home page working

--- src/app/website/website.module.ts
+++ src/app/website/website.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { WebsiteRoutingModule } from './website-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NavbarComponent } from './navbar/navbar.component';
-
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [DashboardComponent, NavbarComponent],
+  declarations: [DashboardComponent, NavbarComponent, FooterComponent],
   imports: [
     CommonModule,
-    WebsiteRoutingModule
+    WebsiteRoutingModule,
+    AngularFontAwesomeModule
   ]
 })
 export class WebsiteModule { }
